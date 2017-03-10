55f32f4b7d25eb084f3d6cbe5fe4ad3f4fbcf6a1
David John <djohn@arch-no.org>
2017-03-10 09:46:59 -0600

    Fixes alias_method_chain deprecation warning

--- lib/devise_duo/mapping.rb
+++ lib/devise_duo/mapping.rb
@@ -1,14 +1,16 @@
-module DeviseDuo
-  module Mapping
-    def self.included(base)
-      base.alias_method_chain :default_controllers, :duo_authenticatable
-    end
-
-    private
+module MappingWithDuoAuth
+  private
     def default_controllers_with_duo_authenticatable(options)
       options[:controllers] ||= {}
       options[:controllers][:passwords] ||= "devise_duo/passwords"
       default_controllers_without_duo_authenticatable(options)
+    end
+end
+
+module DeviseDuo
+  module Mapping
+    def self.included(base)
+      base.prepend(MappingWithDuoAuth)
     end
   end
 end
